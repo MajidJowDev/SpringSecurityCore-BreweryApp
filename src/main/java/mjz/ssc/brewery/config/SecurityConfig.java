893bb2e96b34ad6b4ed4fbecefa957952e3e5357
MajidJowDev <majid.jow@gmail.com>
2023-03-24 19:33:49 +0430

    04- 002 Permit All with URL Pattern Matching Override security auto-configurations to permit all request

---
+++ src/main/java/mjz/ssc/brewery/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package mjz.ssc.brewery.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity  //Because spring security auto-configuration may not find everything on classpath, so it would use conditionals, we can add the security configs here
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authorize -> {
+                    authorize.antMatchers("/", "/webjars/**", "/login", "/resources/**" ).permitAll(); // by this line we permit all requests, (intercepting authorize request). if we do not add "/webjars/**" and "/login" here the bootstrap scripts in webjars won't be loaded on page (loading will encounter error on browser)
+                })
+                .authorizeRequests()
+                .anyRequest().authenticated() // if we want to do exceptions to everything being authenticated, we have to do it before this line (so in this case if we do the antMatchers part after this line we will get an error)
+                .and()
+                .formLogin().and()
+                .httpBasic();
+    }
+
+
+}
---
+++ src/test/java/mjz/ssc/brewery/web/controllers/BaseIT.java
@@ -0,0 +1,46 @@
+package mjz.ssc.brewery.web.controllers;
+
+import mjz.ssc.brewery.repositories.BeerInventoryRepository;
+import mjz.ssc.brewery.repositories.BeerRepository;
+import mjz.ssc.brewery.repositories.CustomerRepository;
+import mjz.ssc.brewery.services.BeerService;
+import mjz.ssc.brewery.services.BreweryService;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+
+
+public abstract class BaseIT {
+    @Autowired
+    WebApplicationContext wac;
+
+    MockMvc mockMvc;
+
+    @MockBean
+    BeerRepository beerRepository;
+
+    @MockBean
+    BeerInventoryRepository beerInventoryRepository;
+
+    @MockBean
+    BreweryService breweryService;
+
+    @MockBean
+    CustomerRepository customerRepository;
+
+    @MockBean
+    BeerService beerService;
+
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(wac)
+                .apply(springSecurity())
+                .build();
+    }
+}
