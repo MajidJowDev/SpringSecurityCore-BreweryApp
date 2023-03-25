3f967dfe563600bb1eb80b748d8e1e341413c390
MajidJowDev <majid.jow@gmail.com>
2023-03-26 00:52:25 +0430

    07- 004 Custom Authentication Filter Extracting username and password from request and create an Authentication Token

---
+++ src/main/java/mjz/ssc/brewery/security/RestHeaderAuthFilter.java
@@ -0,0 +1,52 @@
+package mjz.ssc.brewery.security;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+//Implementing basic Authentication Filter
+@Slf4j
+public class RestHeaderAuthFilter extends AbstractAuthenticationProcessingFilter {
+
+    public RestHeaderAuthFilter(RequestMatcher requiresAuthenticationRequestMatcher) {
+        super(requiresAuthenticationRequestMatcher);
+    }
+
+    //Extracting username and password from request and create an Authentication Token and passing it to the authentication manager
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException, IOException, ServletException {
+        String userName = getUsername(request);
+        String password = getPassword(request);
+
+        //to prevent any downstream problems
+        if(userName == null) {
+            userName = "";
+        }
+
+        if(password == null) {
+            password = "";
+        }
+
+        log.debug("Authenticating User: " + userName);
+
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(userName, password);
+
+        return this.getAuthenticationManager().authenticate(token);
+    }
+
+    private String getUsername(HttpServletRequest request) {
+        return request.getHeader("Api-Key");
+    }
+
+    private String getPassword(HttpServletRequest request) {
+        return request.getHeader("Api-Secret");
+    }
+}
