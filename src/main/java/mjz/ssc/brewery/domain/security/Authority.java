971ef6a8aae92c2a717d7d19b38b4189f7848c81
MajidJowDev <majid.jow@gmail.com>
2023-03-26 13:31:24 +0330

    08- 004 Project Lombok Configuration

--- src/main/java/mjz/ssc/brewery/domain/security/Authority.java
+++ src/main/java/mjz/ssc/brewery/domain/security/Authority.java
@@ -1,8 +1,15 @@
 package mjz.ssc.brewery.domain.security;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.Set;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Entity
 public class Authority {
 
--- src/main/java/mjz/ssc/brewery/domain/security/User.java
+++ src/main/java/mjz/ssc/brewery/domain/security/User.java
@@ -1,10 +1,16 @@
 package mjz.ssc.brewery.domain.security;
 
+import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 
 import javax.persistence.*;
 import java.util.Set;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Entity
 public class User {
 
@@ -14,14 +20,19 @@ public class User {
     private String username;
     private String password;
 
+    @Singular // (this annotation can only be used with builder pattern) so that in the builder pattern we will have a property by the name authority not authorities
     @ManyToMany(cascade = CascadeType.MERGE )
     @JoinTable(name = "user_authority",
             joinColumns = {@JoinColumn(name = "USER_ID", referencedColumnName = "ID")},
             inverseJoinColumns = {@JoinColumn(name = "AUTHORITY_ID", referencedColumnName = "ID")})
     private Set<Authority> authorities;
 
+    @Builder.Default // if we do not add this annotation the builder pattern will set the value of attribs to null by default
     private boolean accountNonExpired = true;
+    @Builder.Default
     private boolean accountNonLocked = true;
+    @Builder.Default
     private boolean credentialsNonExpired = true;
+    @Builder.Default
     private boolean enabled = true;
 }
