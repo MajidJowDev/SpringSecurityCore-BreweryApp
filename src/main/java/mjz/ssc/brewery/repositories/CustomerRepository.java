104d1914a7e5146e85f29baf4abfc26e49ccbb06
MajidJowDev <majid.jow@gmail.com>
2023-03-29 00:13:58 +0330

    11- 008 TDD - Write Failing Tests

--- src/main/java/mjz/ssc/brewery/repositories/CustomerRepository.java
+++ src/main/java/mjz/ssc/brewery/repositories/CustomerRepository.java
@@ -20,9 +20,12 @@ import mjz.ssc.brewery.domain.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 
 public interface CustomerRepository extends JpaRepository<Customer, UUID> {
     List<Customer> findAllByCustomerNameLike(String customerName);
+
+    Optional<Customer> findAllByCustomerName(String customerName);
 }
