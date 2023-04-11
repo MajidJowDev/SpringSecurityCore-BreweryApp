c4df9d20ee5f8289b69150c45c117c407a66663c
MajidJowDev <majid.jow@gmail.com>
2023-04-11 13:59:55 +0330

    16- 012 Exclude Static Resources from Filter

--- src/main/java/mjz/ssc/brewery/security/google/Google2faFilter.java
+++ src/main/java/mjz/ssc/brewery/security/google/Google2faFilter.java
@@ -3,10 +3,14 @@ package mjz.ssc.brewery.security.google;
 
 import lombok.extern.slf4j.Slf4j;
 import mjz.ssc.brewery.domain.security.User;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.boot.autoconfigure.security.servlet.StaticResourceRequest;
 import org.springframework.security.authentication.AuthenticationTrustResolver;
 import org.springframework.security.authentication.AuthenticationTrustResolverImpl;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.GenericFilterBean;
 
@@ -25,10 +29,23 @@ public class Google2faFilter extends GenericFilterBean {
     // we need to use the new impl, because it's not normally in spring context
     private final AuthenticationTrustResolver authenticationTrustResolver = new AuthenticationTrustResolverImpl(); // for checking if the authentication is not anonymous
     private final Google2faFailureHandler google2faFailureHandler = new Google2faFailureHandler();
+    private final RequestMatcher urlIs2fa = new AntPathRequestMatcher("/user/verify2fa");
+    private final RequestMatcher urlResource = new AntPathRequestMatcher("/resources/**");
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        //Spring MVC way to get static resources
+        //Quick way to set up matcher for all general static resources like .js files, .css files, images
+        StaticResourceRequest.StaticResourceRequestMatcher staticResourceRequestMatcher = PathRequest.toStaticResources().atCommonLocations();
+
+        if(urlIs2fa.matches(request) || // to avoid getting into endless loop
+                urlResource.matches(request) ||
+                staticResourceRequestMatcher.matches(request)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
 
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
@@ -44,6 +61,7 @@ public class Google2faFilter extends GenericFilterBean {
                     log.debug("2FA Required");
 
                     google2faFailureHandler.onAuthenticationFailure(request, response, null);
+                    return; // because if we fail on authentication we do not want to continue to execute filterchain
                 }
             }
         }
